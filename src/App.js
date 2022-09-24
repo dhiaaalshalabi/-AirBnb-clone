555aad85f57f1f07d471433203a628c4076451d2
Dhiaa Al-Shalabi <dhiaa.alshalabi@gmail.com>
2022-09-24 06:10:57 +0300

    Use hero section in app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/navbar'
+import Hero from './components/hero'
 import './App.css'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
