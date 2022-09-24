ce86afca244f8429c451b25366dd2665205eb17d
Dhiaa Al-Shalabi <dhiaa.alshalabi@gmail.com>
2022-09-24 06:26:33 +0300

    Use card in app.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './components/navbar'
 import Hero from './components/hero'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card />
     </div>
   )
 }
